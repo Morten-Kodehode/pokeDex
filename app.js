d0ad5f94ad88b3f56147a8bb8988142454405574
Morten Johansson <morten1.kodehode@gmail.com>
2022-11-23 13:53:58 +0100

    finished pokedex

---
+++ app.js
@@ -0,0 +1,42 @@
+const pokedex = document.querySelector("#pokedex");
+const pokeName = document.querySelector("#pokeName");
+const pokeImg = document.querySelector("#pokeImg");
+let statsDiv = document.querySelector("#pokeStats");
+pokeMon = 1;
+
+const btnRandom = () => {
+  pokeMon = Math.floor(Math.random() * 151);
+  renderPokemon();
+};
+
+const btnNext = () => {
+  pokeMon++;
+  renderPokemon();
+};
+
+const btnPrev = () => {
+  pokeMon--;
+  renderPokemon();
+};
+
+const renderPokemon = () => {
+  statsDiv.textContent = "";
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokeMon}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const stats = data.stats;
+
+      pokeName.textContent = `#${data.id}: ${
+        data.name.charAt(0).toUpperCase() + data.name.slice(1)
+      }`;
+      pokeImg.src = data.sprites.other.dream_world.front_default;
+
+      stats.map((x) => {
+        const pokeStat = document.createElement("p");
+        pokeStat.append(`${x.stat.name}: ${x.base_stat}`);
+        statsDiv.append(pokeStat);
+      });
+    });
+};
+
+renderPokemon();
