98bd5a77ad89c571327e8033f426eb31f2dbe607
Morten Johansson <morten1.kodehode@gmail.com>
2022-11-25 13:47:57 +0100

    updated something

--- app.js
+++ app.js
@@ -5,38 +5,36 @@ const statsDiv = document.querySelector("#pokeStats");
 const info = document.querySelector("#pokeInfo");
 const hp = document.querySelector("#pokeHP");
 const pokeType = document.querySelector("#pokeType");
+const moveTop = document.querySelector("#moveTop");
+const moveTopDesc = document.querySelector("#moveTopDesc");
 
-const btnRandom = () => {
-  window.location.reload();
-};
+const btnRandom = () => window.location.reload();
 
 let pokeMon = Math.floor(Math.random() * 151) + 1;
 
 const pokemons = new Request(`https://pokeapi.co/api/v2/pokemon/${pokeMon}`);
-const pokeSpecies = new Request(
-  `https://pokeapi.co/api/v2/pokemon-species/${pokeMon}`
-);
+const pokeSpecies = new Request(`https://pokeapi.co/api/v2/pokemon-species/${pokeMon}`);
 
 const renderPokemon = async () => {
   const pokemon = await fetch(pokemons);
   const pokeData = await pokemon.json();
-
+  
   const types = pokeData.types;
-
-  pokeName.textContent = `${
-    pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)
-  }`;
-  hp.textContent = `${
-    pokeData.stats[0].base_stat
-  } ${pokeData.stats[0].stat.name.toUpperCase()}`;
-
-  pokeImg.src = pokeData.sprites.other.dream_world.front_default;
-
-  pokeType.textContent = `${
-    types[0].type.name.charAt(0).toUpperCase() + types[0].type.name.slice(1)
-  } Pokemon. Length: ${pokeData.height * (0.1).toFixed(1)}m, Weight: ${
-    pokeData.weight * (0.1).toFixed(1)
-  }kg`;
+  
+  pokeName.textContent = `
+  ${pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)}
+  `;
+  
+  hp.textContent = `
+  ${pokeData.stats[0].base_stat} 
+  ${pokeData.stats[0].stat.name.toUpperCase()}`;
+  
+  pokeImg.src = pokeData.sprites.other["official-artwork"].front_default;
+  
+  pokeType.textContent = `
+  ${types[0].type.name.charAt(0).toUpperCase() + types[0].type.name.slice(1)} Pokemon. 
+  Length: ${pokeData.height * (0.1).toFixed(1)}m, 
+  Weight: ${pokeData.weight * (0.1).toFixed(1)}kg`;
 
   const pokeSpeciesRes = await fetch(pokeSpecies);
   const pokeSpeciesData = await pokeSpeciesRes.json();
@@ -44,8 +42,7 @@ const renderPokemon = async () => {
   info.textContent = pokeSpeciesData.flavor_text_entries
     .find(({ language, version }) => {
       return language.name === "en" && version.name === "red";
-    })
-    .flavor_text.replace("\u000c", "\n");
+    }).flavor_text.replace("\u000c", "\n");
 
   pokedex.style.backgroundColor = pokeSpeciesData.color.name;
 };
